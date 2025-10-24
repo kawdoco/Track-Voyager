a913a777aa432118f719e64f06c86c476f32b512
teshanthi-eranga <erangateshanthi@gmail.com>
2025-10-24 11:09:18 +0530

    feat: implement animation path for Voyager events in plot

--- voyager_plot.py
+++ voyager_plot.py
@@ -11,3 +11,17 @@ class VoyagerPlot(FigureCanvas):
         fig = Figure(figsize=(7, 7), facecolor="white")
         super().__init__(fig)
         self.setParent(parent)
+        
+        # Full path arrays
+        self.xs = [e["coords"][0] for e in VOYAGER_EVENTS]
+        self.ys = [e["coords"][1] for e in VOYAGER_EVENTS]
+        self.zs = [e["coords"][2] for e in VOYAGER_EVENTS]
+
+        # For animation
+        self.num_steps = 500
+        self.path_x = np.linspace(self.xs[0], self.xs[-1], self.num_steps)
+        self.path_y = np.linspace(self.ys[0], self.ys[-1], self.num_steps)
+        self.path_z = np.linspace(self.zs[0], self.zs[-1], self.num_steps)
+        self.current_index = 0
+
+        self.init_plot()
