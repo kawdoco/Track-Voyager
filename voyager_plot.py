c515c4a35283001708a63b035d425eff7806b057
Clevin <clevinfer@gmail.com>
2025-10-24 15:54:51 +0530

    fix: correct import statement for VOYAGER_EVENTS in voyager_plot.py

--- voyager_plot.py
+++ voyager_plot.py
@@ -2,7 +2,7 @@
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from Voyager_data import VOYAGER_EVENTS
+from voyager_data import VOYAGER_EVENTS
 
 class VoyagerPlot(FigureCanvas):
     def __init__(self, figure=None, mode="3D", display_points=15):
